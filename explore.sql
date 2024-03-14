fa6a1372b723a18fed0467f2fe48723fb647147f
Ki <ilham_asyilah@yahoo.com.my>
2024-03-14 18:13:28 +0800

    Update explore.sql

--- explore.sql
+++ explore.sql
@@ -1,1 +1,12 @@
+-- Number of types of aisle = 134
+SELECT COUNT(DISTINCT aisle) 
+FROM ic_aisles;
+
+-- Number of types of department = 21
+SELECT COUNT(DISTINCT department) 
+FROM ic_departments;
+
+-- Number of products = 10000
+SELECT COUNT(product_id)
+FROM ic_products;
 
