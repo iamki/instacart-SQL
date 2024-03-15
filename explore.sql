53cf2f7fa152bd94fb1514b651f5043ceeef22ce
Ki <ilham_asyilah@yahoo.com.my>
2024-03-15 17:44:00 +0800

    Update explore.sql

--- explore.sql
+++ explore.sql
@@ -27,3 +27,12 @@ WHERE reordered = 1;
 SELECT COUNT(DISTINCT order_id) 
 FROM ic_order_products_prior
 WHERE reordered = 1;
+
+-- Top 5 most ordered products in Q3
+SELECT curr.product_id, prod.product_name, count(1)
+FROM ic_order_products_curr curr
+  JOIN ic_products prod 
+  ON curr.product_id = prod.product_id
+GROUP BY 1,2
+ORDER BY 3 DESC
+limit 5;
